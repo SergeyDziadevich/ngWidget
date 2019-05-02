04e33aec8eb7e6a7e633231fdcc06f8a388409e1
Sergey_Dziadevich <sergey_dziadevich@epam.com>
2019-05-02 13:52:10 +0300

    moved item-list and item to a separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { SelectItemsComponent } from './select-items/select-items.component';
 import { SelectionComponent } from './selection/selection.component';
 import {FormsModule} from '@angular/forms';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemComponent } from './item-list/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SelectItemsComponent,
-    SelectionComponent
+    SelectionComponent,
+    ItemListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule, FormsModule
--- src/app/select-item.service.ts
+++ src/app/select-item.service.ts
@@ -1,31 +1,63 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { Observable, of } from 'rxjs';
+import {Observable, of} from 'rxjs';
 
-import { Item } from './item';
-import { ITEMS } from './mock-items';
+import {Item} from './item';
+import {ITEMS} from './mock-items';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectItemService {
 
-  selectedItems: Item[] = [
-    {id: 2, title: 'Item', selected: true },
-    {id: 3, title: 'Item', selected: true},
-  ];
+  selectedItems: Item[] = ITEMS.slice().filter(item => item.selected);
 
-  constructor() { }
+  constructor() {
+  }
 
   getItems(): Observable<Item[]> {
     return of(ITEMS);
+  }
+
+  getSelectedItems(): Observable<Item[]> {
+    return of(this.selectedItems);
   }
 
   delete(item: Item): void {
     console.log(`${item.id} was deleted`);
   }
 
-  getSelectedItems(): Observable<Item[]> {
-    return of(this.selectedItems);
+  deleteSelectItems(item: Item): void {
+   // this.selectedItems = this.selectedItems.filter(h => h !== item);
+    console.log(`${item.id} was deleted`);
+  }
+
+  setSelectItems(currentSelectItems: Item[]) {
+
+    this.selectedItems.length = 0;
+    currentSelectItems.map(item => {
+      this.selectedItems.push(item);
+    });
+
+
+ //   console.log(currentSelectItems);
+
+ //   this.selectedItems.length = 0;
+
+    // console.log(this.selectedItems );
+  //  this.selectedItems.length = 0;
+    // currentSelectItems.map(item => {
+    //
+    // });
+
+    // console.log(ITEMS);
+    //
+    // currentSelectItems.map(item => {
+    //   const idx = ITEMS.findIndex(value => +value.id === +item.id);
+    //   ITEMS[idx].selected = item.selected;
+    // });
+    //
+    // console.log(ITEMS);
   }
 }
